b13199abae41d9f4487d186369ceb5bc3f6e05fc
WilderBello <wobelloh@unal.edu.co>
2022-07-19 18:33:48 -0500

    Agregadas vistas con links unidos

---
+++ inicio.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run()
