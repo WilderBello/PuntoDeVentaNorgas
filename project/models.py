c716a0634433a33147a1507c38b07599febc8314
WilderBello <wobelloh@unal.edu.co>
2022-07-20 18:03:02 -0500

    modificada bd

--- project/models.py
+++ project/models.py
@@ -6,3 +6,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    
+class Prueba(UserMixin, db.Model):
+    id_prueba = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(1000))
+    edad = db.Column(db.Integer, foreign_key=True, not_null=True)
