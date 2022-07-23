8ed3b65570be853aabdd622d748157028eef347a
WilderBello <wobelloh@unal.edu.co>
2022-07-23 14:36:43 -0500

    Agregado aviso y reorganizado layout

--- project/auth.py
+++ project/auth.py
@@ -46,6 +46,7 @@ def signup_post():
     user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again
+        flash('Usuario existente')
         return redirect(url_for('auth.signup'))
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
